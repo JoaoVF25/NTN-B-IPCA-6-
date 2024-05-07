b1366126fb352e7352b1a06455feb23a014ccf66
João Victor Fernandes <143907794+JoaoVF25@users.noreply.github.com>
2024-05-07 17:04:42 -0300

    Update ntnb.py

--- ntnb.py
+++ ntnb.py
@@ -18,19 +18,18 @@ print(data)
 # Extrair os dados da coluna 'dados' e criar um novo DataFrame com esses dados
 df_dados = pd.DataFrame(data['dados'])
 
-# Adicionar a coluna 'codigo' com o valor 'NTN-B 35'
+# Colocar o código NTN-B35 na coluna para facilitar o entendimento
 df_dados['codigo'] = 'NTN-B 35'
 
 # Listar as colunas indesejadas para exclusão
 colunas_indesejadas = ['taxaJurosVenda', 'puCompra', 'puBase', 'puVenda', 'pagina', 'tamanho', 'total']
 
-# Verificar se as colunas indesejadas estão presentes no DataFrame antes de tentar removê-las
+# Verificando as colunas antes de ser removida
 colunas_existentes = [col for col in colunas_indesejadas if col in df_dados.columns]
 
-# Remover as colunas existentes no DataFrame
 df_dados = df_dados.drop(colunas_existentes, axis=1)
 
-# Filtrar o DataFrame para manter apenas as linhas onde 'taxaJurosCompra' é maior que 0.02
+# Filtra o Data frame para taxas acima de cada um
 df_dados1 = df_dados.query('taxaJurosCompra >= 0.075').reset_index(drop=True)
 
 df_dados2 = df_dados.query('taxaJurosCompra >= 0.070').reset_index(drop=True)
@@ -64,19 +63,17 @@ print(data)
 # Extrair os dados da coluna 'dados' e criar um novo DataFrame com esses dados
 df_dados = pd.DataFrame(data['dados'])
 
-# Adicionar a coluna 'codigo' com o valor 'NTN-B 35'
 df_dados['codigo'] = 'NTN-B 35'
 
-# Listar as colunas indesejadas para exclusão
+# Excluir essas colunas
 colunas_indesejadas = ['taxaJurosVenda', 'puCompra', 'puBase', 'puVenda', 'pagina', 'tamanho', 'total']
 
 # Verificar se as colunas indesejadas estão presentes no DataFrame antes de tentar removê-las
 colunas_existentes = [col for col in colunas_indesejadas if col in df_dados.columns]
 
-# Remover as colunas existentes no DataFrame
 df_dados = df_dados.drop(colunas_existentes, axis=1)
 
-# Filtrar o DataFrame para manter apenas as linhas onde 'taxaJurosCompra' é maior que 0.02
+# Filtrar o data frame para cada taxa acima de X
 df_dados_1 = df_dados.query('taxaJurosCompra >= 0.075').reset_index(drop=True)
 
 df_dados_2 = df_dados.query('taxaJurosCompra >= 0.070').reset_index(drop=True)
@@ -95,7 +92,6 @@ df_dados_8 = df_dados.query('taxaJurosCompra >= 0.040').reset_index(drop=True)
 
 df_dados.reset_index(drop=True, inplace=True)
 
-# Exibir o DataFrame resultante
 df_dados
 
 data_inicio = "2010-03-10"
@@ -107,9 +103,6 @@ dias_uteis = len(datas)
 
 print(dias_uteis)
 
-
-# Supondo que 'df_dados' seja o DataFrame com as taxas de juros de compra filtradas
-
 # Calcular o total de registros de cada DataFrame
 total_registros1 = len(df_dados1)
 total_registros_1 = len(df_dados_1)
@@ -117,21 +110,16 @@ total_registros_1 = len(df_dados_1)
 # Calcular o total de registros acumulados
 total_registros_acumulado1 = total_registros1 + total_registros_1
 
-# Calcular a porcentagem de dias úteis com taxas de juros de compra acima de 7.5%
 porcentagem_taxas_acima_de_75 = (total_registros_acumulado1 / dias_uteis) * 100
 
-
-# Calcular o total de registros de cada DataFrame
 total_registros2 = len(df_dados2)
 total_registros_2 = len(df_dados_2)
 
-# Calcular o total de registros acumulados
 total_registros_acumulado2 = total_registros2 + total_registros_2
 
-# Calcular a porcentagem de dias úteis com taxas de juros de compra acima de 7.0%
 porcentagem_taxas_acima_de_70 = (total_registros_acumulado2 / dias_uteis) * 100
 
-# Repetir o processo para as demais faixas de taxas de juros
+
 # Para 6.5%
 total_registros3 = len(df_dados3)
 total_registros_3 = len(df_dados_3)
@@ -177,18 +165,17 @@ porcentagem_taxas_acima_de_40 = (total_registros_acumulado8 / dias_uteis) * 100
 porcentagens = [porcentagem_taxas_acima_de_75, porcentagem_taxas_acima_de_70, porcentagem_taxas_acima_de_65, porcentagem_taxas_acima_de_60, porcentagem_taxas_acima_de_55, porcentagem_taxas_acima_de_50, porcentagem_taxas_acima_de_45, porcentagem_taxas_acima_de_40]
 labels = ['>= 7.5%', '>= 7%', '>= 6.5%', '>= 6%', '>= 5.5%', '>= 5%', '>= 4.5%', '>= 4%']
 
-# Criar o gráfico de barras
+# Começar a fazer os gráficos de barra
 plt.figure(figsize=(12, 8))
 bars = plt.bar(labels, porcentagens, color='skyblue')
 
-# Adicionar porcentagens acima das barras
+# Aqui coloca as informações no gra´fico
 for bar, porcentagem in zip(bars, porcentagens):
     plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 0.5, f'{porcentagem:.2f}%', ha='center', va='bottom')
 
-# Adicionar título e rótulos aos eixos
+# TÍTULO E ROTULOS
 plt.title('Porcentagem de dias úteis com taxas de juros acima de determinado valor')
 plt.xlabel('Taxa de Juros (%)')
 plt.ylabel('Porcentagem de Dias Úteis')
 
-# Exibir o gráfico
 plt.show()
